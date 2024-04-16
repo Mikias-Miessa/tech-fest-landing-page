984b62b79833dd0fa625ee50b5767fda5b0b2a81
Mikias Miessa <mikiasmiessa@gmail.com>
2024-04-16 15:52:06 +0300

    fix(add state to form)

--- components/navbar.js
+++ components/navbar.js
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Navbar = () => {
   return (
-    <nav class='bg-white bg-opacity-80  shadow-md backdrop-blur-md px-6  font-Dax fixed z-50 w-screen py-1 '>
-      <div class='flex items-center'>
-        <div class='mr-4'>
+    <nav className='bg-white bg-opacity-80  shadow-md backdrop-blur-md px-6  font-Dax fixed z-50 w-screen py-1 '>
+      <div className='flex items-center'>
+        <div className='mr-4'>
           <img
             src='/logo.png'
             alt='gobeze logo'
