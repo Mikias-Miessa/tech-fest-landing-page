25afa337a971ee8c6d04e6fb6ee0b3630906c6d1
Mikias Miessa <mikiasmiessa@gmail.com>
2024-04-16 14:42:22 +0300

    fix(add link to socials)

--- components/socialLink.js
+++ components/socialLink.js
@@ -3,14 +3,17 @@ import { FaFacebook } from 'react-icons/fa';
 import { FaInstagram } from 'react-icons/fa';
 import { FaTelegram } from 'react-icons/fa';
 import { FaSquareXTwitter } from 'react-icons/fa6';
+import { FaYoutube } from 'react-icons/fa6';
+import { FaTiktok } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
 import SectionTitle from './sectionTitle';
 function SocialLinks() {
   return (
     <div className='flex flex-col justify-center gap-12 items-center  w-[1920px]'>
       <SectionTitle title={'Follow Us'} />
-      <div className='flex justify-center md:gap-24 gap-6 items-center h-full'>
+      <div className='flex justify-center md:gap-24 gap-6 items-center h-full flex-wrap'>
         <a
-          href='https://www.facebook.com/'
+          href='https://www.facebook.com/wingobeze/'
           target='_blank'
           rel='noopener noreferrer'
           className='mr-4 hover:scale-110 duration-200'
@@ -21,7 +24,7 @@ function SocialLinks() {
           />
         </a>
         <a
-          href='https://www.instagram.com/'
+          href='https://www.instagram.com/wingobeze/'
           target='_blank'
           rel='noopener noreferrer'
           className='mr-4 hover:scale-110 duration-200'
@@ -32,7 +35,7 @@ function SocialLinks() {
           />
         </a>
         <a
-          href='https://t.me/'
+          href='https://t.me/wingobeze'
           target='_blank'
           rel='noopener noreferrer'
           className='mr-4 hover:scale-110 duration-200'
@@ -43,7 +46,29 @@ function SocialLinks() {
           />
         </a>
         <a
-          href='https://twitter.com/'
+          href='https://et.linkedin.com/company/gobezeconsult'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='mr-4 hover:scale-110 duration-200'
+        >
+          <FaLinkedin
+            color='#0077B5'
+            className='font-extrabold text-4xl md:text-6xl'
+          />
+        </a>
+        <a
+          href='https://www.tiktok.com/@wingobeze?_t=8lZg0QUgovV&_r=1'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='mr-4 hover:scale-110 duration-200'
+        >
+          <FaTiktok
+            color='#000000'
+            className='font-extrabold text-4xl md:text-6xl'
+          />
+        </a>
+        <a
+          href='https://twitter.com/traingobeze'
           target='_blank'
           rel='noopener noreferrer'
         >
